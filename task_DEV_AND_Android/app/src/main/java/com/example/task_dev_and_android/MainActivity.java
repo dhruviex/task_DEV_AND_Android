d09fd30fdf361d96246c57d96a07e286014d67cd
dhruviex <C0846368@mylambton.ca>
2022-06-17 14:47:44 -0400

    Added the Sound Record
    Added the code for the recoridng , play and save the sound file in the folder

--- task_DEV_AND_Android/app/src/main/java/com/example/task_dev_and_android/MainActivity.java
+++ task_DEV_AND_Android/app/src/main/java/com/example/task_dev_and_android/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.task_dev_and_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.Manifest;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.example.task_dev_and_android.databinding.ActivityMainBinding;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskC
     private ActivityMainBinding binding;
     TaskAdapter mAdapter;
     ArrayList<TaskModel> taskList;
+    private static int MICROPHONE_PERMISSION_CODE = 200;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskC
 
         addCustomTaskList();
         setAdapter();
+        if(isMicrophonePresent()){
+            getMicrophonePermission();
+        }
     }
 
     private void addCustomTaskList() {
@@ -56,5 +71,66 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskC
 
     public void onCheckboxClicked(View view) {
         boolean checked = ((CheckBox) view).isChecked();
+    }
+
+
+    //Sound Recording Code
+    public void btnRecordPressed(View v){
+
+     try {
+
+
+         mediaRecorder = new MediaRecorder();
+         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+         mediaRecorder.prepare();
+         mediaRecorder.start();
+
+         Toast.makeText(this,"Recording Started",Toast.LENGTH_LONG).show();
+
+     }
+     catch(Exception e){
+         e.printStackTrace();
+     }
+    }
+    public void btnPlayPressed(View v){
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getRecordingFile());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }catch (Exception e) {
+           e.printStackTrace();
+        }
+        Toast.makeText(this,"Recording Playing",Toast.LENGTH_LONG).show();
+    }
+    public void btnStopPressed(View v){
+
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+
+        Toast.makeText(this,"Recording Stopped",Toast.LENGTH_LONG).show();
+    }
+    private boolean isMicrophonePresent() {
+        if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private void getMicrophonePermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.RECORD_AUDIO},MICROPHONE_PERMISSION_CODE);
+        }
+    }
+    private String getRecordingFile(){
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File musicDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File file  =new File(musicDirectory, "RecordingFile" + ".mp3");
+        return file.getPath();
     }
 }
---
+++ task_DEV_AND_Android/app/src/main/java/com/example/task_dev_and_android/NewTaskActivity.java
@@ -0,0 +1,14 @@
+package com.example.task_dev_and_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
+
+public class NewTaskActivity {
+
+
+}
